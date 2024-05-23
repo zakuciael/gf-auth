38a1c9228772cc9b4bd11b1461da6338132499a6
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-05-23 10:55:44 +0200

    build(nix): add dev shell

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A blazingly fast client SDK for Gameforge Auth APIs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        fenix = inputs'.fenix.packages;
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "gf-auth";
+          nativeBuildInputs = [
+            (fenix.stable.withComponents [
+              "cargo"
+              "rustc"
+              "rust-src"
+              "clippy"
+              "rustfmt"
+            ])
+          ];
+        };
+      };
+    };
+}
