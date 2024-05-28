7ee1eb6bb2f002a3f3d356be4a45d050d24f1f88
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-05-28 19:51:09 +0200

    feat(gf-auth): allow editing vector in `Fingerprint` struct

--- crates/gf-auth/src/blackbox/fingerprint.rs
+++ crates/gf-auth/src/blackbox/fingerprint.rs
@@ -1,5 +1,29 @@
-use chrono::{DateTime, Utc};
+use std::error::Error;
+use std::ops::Add;
+
+use chrono::{DateTime, TimeDelta, Utc};
+use rand::distributions::uniform::UniformSampler;
+use rand::distributions::Uniform;
+use rand::Rng;
 use serde::{Deserialize, Serialize};
+
+const VECTOR_CONTENT_LENGTH: usize = 100;
+const GAME_LENGTH: usize = 27;
+
+pub type FingerprintResult<T, E = FingerprintError> = Result<T, E>;
+pub type FingerprintVector<'a> = (&'a str, DateTime<Utc>);
+
+#[derive(thiserror::Error, Debug)]
+pub enum FingerprintError {
+  #[error("json parse error: {0}")]
+  ParseJson(#[from] serde_json::Error),
+
+  #[error("input/output error: {0}")]
+  IO(#[from] std::io::Error),
+
+  #[error("conversion error: {0}")]
+  Convert(String),
+}
 
 #[derive(Serialize, Deserialize)]
 pub(crate) struct BlackboxRequest {
@@ -70,11 +94,67 @@ pub(crate) struct Fingerprint {
   request: Option<BlackboxRequest>,
 }
 
+impl Fingerprint {
+  fn generate_vector() -> String {
+    let rng = rand::thread_rng();
+    let content: String = rng
+      .sample_iter(Uniform::new(32, 126))
+      .take(VECTOR_CONTENT_LENGTH)
+      .map(char::from)
+      .collect();
+    let time = Utc::now();
+    format!("{} {}", content, time.timestamp_millis())
+  }
+
+  fn set_request(&mut self, request: BlackboxRequest) {
+    self.request = Some(request);
+  }
+
+  fn update_vector(&mut self) -> FingerprintResult<()> {
+    let current_time = Utc::now();
+    let (content, old_time) = self.unpack_vector()?;
+
+    let content = if old_time.add(TimeDelta::milliseconds(1000)) < current_time {
+      let mid = (&content[1..]).to_owned();
+      let rand_char = rand::thread_rng().sample(Uniform::new(32, 126)).to_string();
+      mid + &rand_char
+    } else {
+      content.to_string()
+    };
+
+    self.pack_vector((&content, current_time));
+    Ok(())
+  }
+
+  fn pack_vector(&mut self, vector: FingerprintVector) {
+    self.vector = format!("{} {}", vector.0, vector.1.timestamp_millis())
+  }
+
+  fn unpack_vector(&self) -> FingerprintResult<FingerprintVector> {
+    let index = self
+      .vector
+      .find(' ')
+      .ok_or_else(|| FingerprintError::Convert("invalid vector".to_owned()))?;
+    let time: i64 = (&self.vector[index + 1..])
+      .parse()
+      .map_err(|_| FingerprintError::Convert("vector time is not a valid integer".to_owned()))?;
+    let time = DateTime::from_timestamp_millis(time)
+      .ok_or_else(|| FingerprintError::Convert("vector time is not a valid date".to_owned()))?;
+
+    Ok((&self.vector[0..index], time))
+  }
+
+  fn get_server_time() -> DateTime<Utc> {
+    Utc::now()
+  }
+}
+
 #[cfg(test)]
 mod tests {
-  use crate::blackbox::fingerprint::Fingerprint;
   use std::error::Error;
   use std::fs;
+
+  use crate::blackbox::fingerprint::Fingerprint;
 
   #[test]
   fn parse_fingerprint() -> Result<(), Box<dyn Error>> {
@@ -91,6 +171,25 @@ mod tests {
       .expect("Failed to read fingerprint file.");
 
     serde_json::from_str::<Fingerprint>(&file)?;
+    Ok(())
+  }
+
+  #[test]
+  fn unpack_vector() -> Result<(), Box<dyn Error>> {
+    let file =
+      fs::read_to_string("./resources/fingerprint1.json").expect("Failed to read fingerprint file");
+
+    println!(
+      "{:?}",
+      serde_json::from_str::<Fingerprint>(&file)?.unpack_vector()
+    );
+    Ok(())
+  }
+
+  #[test]
+  fn test() -> Result<(), Box<dyn Error>> {
+    println!("{}", Fingerprint::generate_vector());
+
     Ok(())
   }
 }
