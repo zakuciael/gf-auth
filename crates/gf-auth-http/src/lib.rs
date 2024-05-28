3eca991c97190376a2434090587b48a27af09555
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-05-28 19:48:21 +0200

    refactor(http-client): export ClientError type

--- crates/gf-auth-http/src/lib.rs
+++ crates/gf-auth-http/src/lib.rs
@@ -1,5 +1,12 @@
 #[cfg(any(feature = "client-reqwest", feature = "client-ureq"))]
 #[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
+extern crate reqwest as http_reqwest;
+#[cfg(feature = "client-ureq")]
+#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
+extern crate ureq as http_ureq;
+
+#[cfg(any(feature = "client-reqwest", feature = "client-ureq"))]
+#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 pub use crate::common::{BaseHttpClient, CustomCertHttpClient, Form, Headers, HttpError, Query};
 
 #[cfg(feature = "client-ureq")]
@@ -8,6 +15,13 @@ pub use self::ureq::UreqClient as HttpClient;
 #[cfg(feature = "client-reqwest")]
 #[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
 pub use crate::reqwest::ReqwestClient as HttpClient;
+
+#[cfg(any(feature = "client-reqwest", feature = "client-ureq"))]
+#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
+pub use http_reqwest::Error as ClientError;
+#[cfg(feature = "client-ureq")]
+#[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
+pub use http_ureq::Error as ClientError;
 
 #[cfg(feature = "client-reqwest")]
 #[cfg(not(all(feature = "client-reqwest", feature = "client-ureq")))]
